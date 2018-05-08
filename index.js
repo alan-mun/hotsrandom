3067dcb12a4f0d5fc434314950c08b19237ecb5f
Xynced <awgorinov@gmail.com>
2018-05-08 03:23:54 +0300

    generator

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+const params = require('./params.js')
+const heroes = require('./heroes.js')
+
+function getRandom(max) {
+    return Math.floor(Math.random() * max)
+}
+
+function get_heroes() {
+    const length = heroes.length
+    let ids = []
+    let result = []
+    for (let i = 0; i < params.heroes; ++i) {
+        let next = getRandom(length)
+        while (ids.includes(next)) {
+            next = getRandom(length)
+        }
+        ids.push(next)
+        result.push(heroes[next])
+    }
+    return result
+}
+
+console.log(get_heroes())
---
+++ params.js
@@ -0,0 +1,3 @@
+module.exports = {
+    heroes: 3
+}
